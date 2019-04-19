f1df40940b46daf928a6bb8e506539e206f0ae85
burkus <andrew@burkus.xyz>
2019-04-19 15:25:24 -0500

    added copy-paste of images

--- app/components/MemePreview.js
+++ app/components/MemePreview.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+const { clipboard } = require('electron');
 import bulma from 'bulma/css/bulma.css';
 import styles from './MemePreview.css';
 
@@ -9,18 +10,42 @@ type Props = {
     name: string
 };
 
-export default function MemePreview (props: Props) {
-    const {src, width, height} = props;
-    return (
-        <div className={bulma.box}>
-            <div className={bulma.level}>
-            <figure className={bulma['level-item'], bulma['level-left']}>
-                <img src={src} width={width} height={height} alt={"whoops, couldn't load"}></img>
-            </figure>
-            <div className={bulma['level-right']}>
-                    <p>{props.name}</p>
+export default class MemePreview extends Component<Props> {
+    constructor(props: Props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        clipboard.writeImage(this.props.src);
+    }
+
+    render() {
+        const {src, width, height} = this.props;
+        return (
+            <div className={bulma.box}>
+                <div className={bulma.level}>
+                <figure className={bulma['level-item'], bulma['level-left']}>
+                    <img
+                        src={src}
+                        width={width}
+                        height={height}
+                        alt={"whoops, couldn't load"}
+                        >
+                    </img>
+                </figure>
+                <p>{this.props.name}</p>
+                    <div className={bulma['level-right']}>
+                        
+                        <button
+                            className={bulma.button}
+                            onClick={this.handleClick}
+                            >
+                            Copy
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
